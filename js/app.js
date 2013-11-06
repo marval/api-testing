e620032c730bb481be7925b6be35c8f74299ae51
Martin Valov <martin.valov@gmail.com>
2013-11-06 23:11:03 +0100

    First "working" version

--- js/app.js
+++ js/app.js
@@ -11,3 +11,19 @@ app.config(function($routeProvider, $compileProvider) {
   $compileProvider.urlSanitizationWhitelist(/^\s*(https?|ftp|mailto|file|magnet):/);
 
 });
+
+app.factory("TestingService", ['$http', function (async) {
+      return {
+            testAPI: function(httpMethod, url, path, postData) {
+              var promise = async({method: httpMethod, url: 'http://' + url + '/' + path, withCredentials: true, data: JSON.stringify(postData)})
+                .success(function (data, status, headers, config) {
+                  return data;
+                })
+                .error(function (data, status, headers, config) {
+                  return {"status": false};
+                });
+
+              return promise;
+            }
+      };
+}]);
--- js/mainCtrl.js
+++ js/mainCtrl.js
@@ -1,7 +1,21 @@
-app.controller("MainCtrl", function($scope){
-    $scope.results = [];
-    $scope.watchedList = [];
-    $scope.user = {};
-    $scope.filterBy = "all";
+app.controller("MainCtrl", function($scope, TestingService){
+    $scope.httpMethod = "GET";
+    $scope.url = "";
+    $scope.path = "";
+    $scope.postData = "";
+
+    $scope.result = {};
+
+    $scope.fireACall = function() {
+        $("#submitButton").button('loading');
+        var json = {};
+        if ($scope.postData.length > 0) {
+            json = JSON.parse($scope.postData);
+        }
+        TestingService.testAPI($scope.httpMethod, $scope.url, $scope.path, json).then(function (promise) {
+            $scope.result = promise;
+            $("#submitButton").button('reset');
+        });
+    };
 
 });
