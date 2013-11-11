d2a43f70edc0dc68e49d44e4875af7c5f7a47592
Martin Valov <martin.valov@gmail.com>
2013-11-11 10:24:21 +0100

    Enabling CORS by default

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 var app = angular.module('APITesting', []);
 
-app.config(function($routeProvider, $compileProvider) {
+app.config(function($routeProvider, $compileProvider, $httpProvider) {
 
   $routeProvider.when('/', {
     templateUrl: 'main.html',
@@ -8,14 +8,18 @@ app.config(function($routeProvider, $compileProvider) {
   });
 
   $routeProvider.otherwise({ redirectTo: '/' });
-  $compileProvider.urlSanitizationWhitelist(/^\s*(https?|ftp|mailto|file|magnet):/);
+
+
+  $httpProvider.defaults.useXDomain = true;
+  $httpProvider.defaults.withCredentials = true;
+  delete $httpProvider.defaults.headers.common['X-Requested-With'];
 
 });
 
 app.factory("TestingService", ['$http', function (async) {
       return {
             testAPI: function(httpMethod, url, path, postData) {
-              var promise = async({method: httpMethod, url: 'http://' + url + '/' + path, withCredentials: true, data: JSON.stringify(postData)})
+              var promise = async({method: httpMethod, url: 'http://' + url + '/' + path, withCredentials: true, data: JSON.stringify(postData), useXDomain: true})
                 .success(function (data, status, headers, config) {
                   return data;
                 })
